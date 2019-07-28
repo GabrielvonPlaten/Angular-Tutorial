3b97c885a3dc16094b46508782450b3443e2e551
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-28 23:44:14 +0200

    Constructor, methods & string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  name: string = '';
+
+  // Constructor runs when the component is initialized
+  constructor() {
+    console.log(123);
+  }
+
+  calc = (x: number, y: number): number => x * y;
+
+  changeName = (name: string): void => {
+    this.name = name;
+    console.log(this.name);
+  };
 }
