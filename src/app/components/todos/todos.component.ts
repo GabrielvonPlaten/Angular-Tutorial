b14f87382b3a6272bd5069728240f9ed9cae77b7
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-28 23:59:57 +0200

    Create Todo & Todo-Item components

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.sass']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 0,
+        title: 'Title One',
+        completed: false
+      },
+      {
+        id: 1,
+        title: 'Title Two',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Title Three',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Title Four',
+        completed: false
+      }
+    ];
+  }
+}
